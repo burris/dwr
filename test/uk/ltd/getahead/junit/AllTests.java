547cb71a15f6ce9cef61d1cd50218ba24b503fb5
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2005-07-19 08:45:30 +0000

    removing warnings
    git-svn-id: http://svn.directwebremoting.org/trunk@240 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- test/uk/ltd/getahead/junit/AllTests.java
+++ test/uk/ltd/getahead/junit/AllTests.java
@@ -3,17 +3,20 @@ package uk.ltd.getahead.junit;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+/**
+ * @author Joe Walker [joe at getahead dot ltd dot uk]
+ */
 public class AllTests
 {
-
+    /**
+     * @return test suite
+     */
     public static Test suite()
     {
-        TestSuite suite = new TestSuite("Test for uk.ltd.getahead.junit");
+        TestSuite suite = new TestSuite("Test for uk.ltd.getahead.junit"); //$NON-NLS-1$
         //$JUnit-BEGIN$
         suite.addTestSuite(JavascriptUtilTest.class);
         //$JUnit-END$
         return suite;
     }
-
 }
-
