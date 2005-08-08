65039adb209564399733f05f8bf7427912b2e5da
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2005-08-08 10:05:56 +0000

    better error handling under securitymanager
    git-svn-id: http://svn.directwebremoting.org/trunk@266 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/uk/ltd/getahead/dwr/impl/DefaultProcessor.java
+++ java/uk/ltd/getahead/dwr/impl/DefaultProcessor.java
@@ -542,124 +542,132 @@ public class DefaultProcessor implements Processor
      */
     protected void doExec(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
-        ExecuteQuery eq = new ExecuteQuery(creatorManager, converterManager, accessControl);
-
-        Calls calls;
         try
         {
-            calls = eq.execute(req);
+            ExecuteQuery eq = new ExecuteQuery(creatorManager, converterManager, accessControl);
+
+            Calls calls;
+            try
+            {
+                calls = eq.execute(req);
+            }
+            catch (Exception ex)
+            {
+                // This only catches exceptions parsing the request. All execution
+                // exceptions are returned inside the Call POJOs.
+                if (log.isDebugEnabled())
+                {
+                    ex.printStackTrace();
+
+                    log.debug("Parse error: " + ex); //$NON-NLS-1$
+                    log.debug("- User Agent: " + req.getHeader(HEADER_USER_AGENT)); //$NON-NLS-1$
+                    log.debug("- Remote IP:  " + req.getRemoteAddr()); //$NON-NLS-1$
+                    log.debug("- Request URL:" + req.getRequestURL()); //$NON-NLS-1$
+                    log.debug("- Query:      " + req.getQueryString()); //$NON-NLS-1$
+                    log.debug("- Method:     " + req.getMethod()); //$NON-NLS-1$
+                    log.debug("- Body: {"); //$NON-NLS-1$
+
+                    int lines = 0;
+                    BufferedReader in = req.getReader();
+                    while (in != null && lines < 100)
+                    {
+                        String line = in.readLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        log.debug("-   " + line); //$NON-NLS-1$
+                        lines++;
+                    }
+
+                    log.debug("- }" + req.getMethod()); //$NON-NLS-1$
+                }
+
+                resp.setContentType(MIME_HTML);
+                PrintWriter out = resp.getWriter();
+                out.println("//<script type='text/javascript'>"); //$NON-NLS-1$
+                out.println("DWREngine._handleWarning('Your browser sent a request that could not be understood.\\nIf you understand how Javascript works in your browser, please help us fix the problem.\\nSee the mailing lists at http://www.getahead.ltd.uk/dwr/ for more information.');"); //$NON-NLS-1$
+                out.println("//</script>"); //$NON-NLS-1$
+                out.flush();
+                return;
+            }
+
+            for (int i = 0; i < calls.getCallCount(); i++)
+            {
+                Call call = calls.getCall(i);
+                if (call.getThrowable() != null)
+                {
+                    log.warn("Erroring: id[" + call.getId() + "] message[" + call.getThrowable().toString() + ']'); //$NON-NLS-1$ //$NON-NLS-2$
+                }
+                else
+                {
+                    log.debug("Returning: id[" + call.getId() + "] init[" + call.getReply().getInitCode() + "] assign[" + call.getReply().getAssignCode() + "] xhr[" + calls.isXhrMode() + ']'); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+                }
+            }
+    
+            // We build the answer up in a StringBuffer because that makes is easier
+            // to debug, and because that's only what the compiler does anyway.
+            StringBuffer buffer = new StringBuffer();
+    
+            // if we are in html (iframe mode) we need to direct script to the parent
+            String prefix = calls.isXhrMode() ? "" : "window.parent."; //$NON-NLS-1$ //$NON-NLS-2$
+    
+            // iframe mode starts as HTML, so get into script mode
+            if (!calls.isXhrMode())
+            {
+                buffer.append("<script type='text/javascript'>\n"); //$NON-NLS-1$
+            }
+    
+            for (int i = 0; i < calls.getCallCount(); i++)
+            {
+                Call call = calls.getCall(i);
+                if (call.getThrowable() != null)
+                {
+                    String output = jsutil.escapeJavaScript(call.getThrowable().toString());
+    
+                    buffer.append(prefix);
+                    buffer.append("DWREngine._handleError('"); //$NON-NLS-1$
+                    buffer.append(call.getId());
+                    buffer.append("', '"); //$NON-NLS-1$
+                    buffer.append(output);
+                    buffer.append("');\n"); //$NON-NLS-1$
+                }
+                else
+                {
+                    buffer.append(call.getReply().getInitCode());
+                    buffer.append('\n');
+    
+                    buffer.append(prefix);
+                    buffer.append("DWREngine._handleResponse('"); //$NON-NLS-1$
+                    buffer.append(call.getId());
+                    buffer.append("', "); //$NON-NLS-1$
+                    buffer.append(call.getReply().getAssignCode());
+                    buffer.append(");\n"); //$NON-NLS-1$
+                }
+            }
+    
+            // iframe mode needs to get out of script mode
+            if (!calls.isXhrMode())
+            {
+                buffer.append("</script>\n"); //$NON-NLS-1$
+            }
+    
+            String reply = buffer.toString();
+            log.debug(reply);
+    
+            // LocalUtil.addNoCacheHeaders(resp);
+            resp.setContentType(calls.isXhrMode() ? MIME_XML : MIME_HTML);
+            PrintWriter out = resp.getWriter();
+            out.print(reply);
+            out.flush();
         }
         catch (Exception ex)
         {
-            // This only catches exceptions parsing the request. All execution
-            // exceptions are returned inside the Call POJOs.
-            if (log.isDebugEnabled())
-            {
-                ex.printStackTrace();
-
-                log.debug("Parse error: " + ex); //$NON-NLS-1$
-                log.debug("- User Agent: " + req.getHeader(HEADER_USER_AGENT)); //$NON-NLS-1$
-                log.debug("- Remote IP:  " + req.getRemoteAddr()); //$NON-NLS-1$
-                log.debug("- Request URL:" + req.getRequestURL()); //$NON-NLS-1$
-                log.debug("- Query:      " + req.getQueryString()); //$NON-NLS-1$
-                log.debug("- Method:     " + req.getMethod()); //$NON-NLS-1$
-                log.debug("- Body: {"); //$NON-NLS-1$
-
-                int lines = 0;
-                BufferedReader in = req.getReader();
-                while (in != null && lines < 100)
-                {
-                    String line = in.readLine();
-
-                    if (line == null)
-                    {
-                        break;
-                    }
-
-                    log.debug("-   " + line); //$NON-NLS-1$
-                    lines++;
-                }
-
-                log.debug("- }" + req.getMethod()); //$NON-NLS-1$
-            }
-
-            resp.setContentType(MIME_HTML);
-            PrintWriter out = resp.getWriter();
-            out.println("//<script type='text/javascript'>"); //$NON-NLS-1$
-            out.println("DWREngine._handleWarning('Your browser sent a request that could not be understood.\\nIf you understand how Javascript works in your browser, please help us fix the problem.\\nSee the mailing lists at http://www.getahead.ltd.uk/dwr/ for more information.');"); //$NON-NLS-1$
-            out.println("//</script>"); //$NON-NLS-1$
-            out.flush();
-            return;
+            log.error("Failure in doExec()",ex); //$NON-NLS-1$
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, ex.toString());
         }
-
-        for (int i = 0; i < calls.getCallCount(); i++)
-        {
-            Call call = calls.getCall(i);
-            if (call.getThrowable() != null)
-            {
-                log.warn("Erroring: id[" + call.getId() + "] message[" + call.getThrowable().toString() + ']'); //$NON-NLS-1$ //$NON-NLS-2$
-            }
-            else
-            {
-                log.debug("Returning: id[" + call.getId() + "] init[" + call.getReply().getInitCode() + "] assign[" + call.getReply().getAssignCode() + "] xhr[" + calls.isXhrMode() + ']'); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-            }
-        }
-
-        // We build the answer up in a StringBuffer because that makes is easier
-        // to debug, and because that's only what the compiler does anyway.
-        StringBuffer buffer = new StringBuffer();
-
-        // if we are in html (iframe mode) we need to direct script to the parent
-        String prefix = calls.isXhrMode() ? "" : "window.parent."; //$NON-NLS-1$ //$NON-NLS-2$
-
-        // iframe mode starts as HTML, so get into script mode
-        if (!calls.isXhrMode())
-        {
-            buffer.append("<script type='text/javascript'>\n"); //$NON-NLS-1$
-        }
-
-        for (int i = 0; i < calls.getCallCount(); i++)
-        {
-            Call call = calls.getCall(i);
-            if (call.getThrowable() != null)
-            {
-                String output = jsutil.escapeJavaScript(call.getThrowable().toString());
-
-                buffer.append(prefix);
-                buffer.append("DWREngine._handleError('"); //$NON-NLS-1$
-                buffer.append(call.getId());
-                buffer.append("', '"); //$NON-NLS-1$
-                buffer.append(output);
-                buffer.append("');\n"); //$NON-NLS-1$
-            }
-            else
-            {
-                buffer.append(call.getReply().getInitCode());
-                buffer.append('\n');
-
-                buffer.append(prefix);
-                buffer.append("DWREngine._handleResponse('"); //$NON-NLS-1$
-                buffer.append(call.getId());
-                buffer.append("', "); //$NON-NLS-1$
-                buffer.append(call.getReply().getAssignCode());
-                buffer.append(");\n"); //$NON-NLS-1$
-            }
-        }
-
-        // iframe mode needs to get out of script mode
-        if (!calls.isXhrMode())
-        {
-            buffer.append("</script>\n"); //$NON-NLS-1$
-        }
-
-        String reply = buffer.toString();
-        log.debug(reply);
-
-        // LocalUtil.addNoCacheHeaders(resp);
-        resp.setContentType(calls.isXhrMode() ? MIME_XML : MIME_HTML);
-        PrintWriter out = resp.getWriter();
-        out.print(reply);
-        out.flush();
     }
 
     /**
