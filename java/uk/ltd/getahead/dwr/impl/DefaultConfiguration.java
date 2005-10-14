a095a708f71428f432c5c2b2defff8a7fec4a55a
bramsmeets <bramsmeets@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2005-10-14 22:49:33 +0000

    * Added a getter method for the AJAX filter manager
    git-svn-id: http://svn.directwebremoting.org/trunk@305 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/uk/ltd/getahead/dwr/impl/DefaultConfiguration.java
+++ java/uk/ltd/getahead/dwr/impl/DefaultConfiguration.java
@@ -572,6 +572,15 @@ public class DefaultConfiguration implements Configuration
 
     /**
      * Accessor for the AjaxFilterManager
+     * @return Returns the AjaxFilterManager.
+     */
+    public AjaxFilterManager getAjaxFilterManager()
+    {
+        return ajaxFilterManager;
+    }
+
+    /**
+     * Accessor for the AjaxFilterManager
      * @param ajaxFilterManager The AjaxFilterManager to set.
      */
     public void setAjaxFilterManager(AjaxFilterManager ajaxFilterManager)
