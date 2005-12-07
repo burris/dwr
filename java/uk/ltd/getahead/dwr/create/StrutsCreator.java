a451b43f181066c1d22334f506288a9764a53c7c
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2005-12-07 17:22:34 +0000

    typo in comments
    git-svn-id: http://svn.directwebremoting.org/trunk@433 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/uk/ltd/getahead/dwr/create/StrutsCreator.java
+++ java/uk/ltd/getahead/dwr/create/StrutsCreator.java
@@ -41,7 +41,7 @@ public class StrutsCreator extends AbstractCreator implements Creator
     }
 
     /* (non-Javadoc)
-     * @see uk.ltd.getahead.dwr.Creator#getType()q
+     * @see uk.ltd.getahead.dwr.Creator#getType()
      */
     public Class getType()
     {
