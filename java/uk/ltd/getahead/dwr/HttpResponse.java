5be4c0a96a75d5086a9ac25c6b1760e08c096743
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-02-05 13:31:34 +0000

    Copyright and comments
    git-svn-id: http://svn.directwebremoting.org/trunk@502 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/uk/ltd/getahead/dwr/HttpResponse.java
+++ java/uk/ltd/getahead/dwr/HttpResponse.java
@@ -1,11 +1,30 @@
+/*
+ * Copyright 2005 Joe Walker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ltd.getahead.dwr;
 
 /**
+ * A simple class to encapsulate an HTTP response.
+ * Currently all we need is a body and a mime type, however in the future there
+ * may be other things we need to add.
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
 public class HttpResponse
 {
     /**
+     * Accessor for the HTTP body
      * @return the body
      */
     public byte[] getBody()
@@ -14,6 +33,7 @@ public class HttpResponse
     }
 
     /**
+     * Accessor for the HTTP body
      * @param body the body to set
      */
     public void setBody(byte[] body)
@@ -22,16 +42,18 @@ public class HttpResponse
     }
 
     /**
-     * @param replyString
+     * Accessor for the HTTP body
+     * @param body the body to set
      */
-    public void setBody(String replyString)
+    public void setBody(String body)
     {
         // TODO: Think about encoding. The servlet spec says it uses a default
         // value of "ISO-8859-1".
-        this.body = replyString.getBytes();
+        this.body = body.getBytes();
     }
 
     /**
+     * Accessor for the MimeType that the body is in
      * @return the mimeType
      */
     public String getMimeType()
@@ -40,6 +62,7 @@ public class HttpResponse
     }
 
     /**
+     * Accessor for the MimeType that the body is in
      * @param mimeType the mimeType to set
      */
     public void setMimeType(String mimeType)
@@ -47,6 +70,13 @@ public class HttpResponse
         this.mimeType = mimeType;
     }
 
+    /**
+     * The data in the HTTP response
+     */
     private byte[] body;
+
+    /**
+     * The MimeType of the body
+     */
     private String mimeType;
 }
