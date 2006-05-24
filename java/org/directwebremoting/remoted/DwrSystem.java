ff420206528c9096d3679cd54df88a8bac0e3a25
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-05-24 03:58:03 +0000

    refactoring to separate conduits
    git-svn-id: http://svn.directwebremoting.org/trunk@670 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/org/directwebremoting/remoted/DwrSystem.java
+++ java/org/directwebremoting/remoted/DwrSystem.java
@@ -76,7 +76,7 @@ public class DwrSystem
         WebContext context = WebContextFactory.get();
         Container container = context.getContainer();
 
-        final ScriptSession scriptSession = context.getScriptSession();
+        ScriptSession scriptSession = context.getScriptSession();
         ServerLoadMonitor monitor = (ServerLoadMonitor) container.getBean(ServerLoadMonitor.class.getName());
         long sleepTime = monitor.timeWithinPollPreStream();
 
@@ -96,14 +96,15 @@ public class DwrSystem
             if (continuation != null)
             {
                 useSleep = false;
+                ScriptConduit listener = null;
 
                 try
                 {
                     // create a listener 
-                    ScriptConduit listener = (ScriptConduit) getObject.invoke(continuation, new Object[0]);
+                    listener = (ScriptConduit) getObject.invoke(continuation, new Object[0]);
                     if (listener == null)
                     {
-                        listener = new SystemScriptConduit(continuation);
+                        listener = new ResumeContinuationScriptConduit(continuation);
                         setObject.invoke(continuation, new Object[] { listener });
                     }
                     scriptSession.addScriptConduit(listener);
@@ -124,40 +125,26 @@ public class DwrSystem
                     log.warn("Exception", ex); //$NON-NLS-1$
                     useSleep = true;
                 }
+                finally
+                {
+                    if (listener != null)
+                    {
+                        scriptSession.removeScriptConduit(listener);
+                    }
+                }
             }
 
             if (useSleep)
             {
-                // create a listener // TODO avoid the expense of creation and registration
-                ScriptConduit listener = new ScriptConduit()
-                {
-                    public boolean addScript(String script)
-                    {
-                        try
-                        {
-                            synchronized (scriptSession.getScriptLock())
-                            {
-                                scriptSession.getScriptLock().notifyAll();
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            log.warn("Failed to notify all ScriptSession users", ex); //$NON-NLS-1$
-                        }
-
-                        // just listening!
-                        return false;
-                    }
-
-                    public void flush()
-                    {
-                    }
-                };
-                scriptSession.addScriptConduit(listener);
+                Object lock = scriptSession.getScriptLock();
+                // TODO avoid the expense of creation and registration
+                ScriptConduit listener = new NotifyOnlyScriptConduit(lock);
 
                 // The comet part of a poll request
                 try
                 {
+                    scriptSession.addScriptConduit(listener);
+
                     scriptSession.getScriptLock().wait(sleepTime);
                 }
                 catch (InterruptedException ex)
@@ -172,14 +159,62 @@ public class DwrSystem
         }
     }
 
-    private static final class SystemScriptConduit implements ScriptConduit
+    /**
+     * Implementation of ScriptConduit that simply calls <code>notifyAll()</code>
+     * if a script is added.
+     * No actual script adding is done here.
+     * Useful in conjunction with a preStreamWait to
+     */
+    private static final class NotifyOnlyScriptConduit implements ScriptConduit
     {
-        private final Object continuation;
+        /**
+         * @param lock Object to wait and notifyAll with
+         */
+        private NotifyOnlyScriptConduit(Object lock)
+        {
+            this.lock = lock;
+        }
 
+        /* (non-Javadoc)
+         * @see org.directwebremoting.ScriptConduit#addScript(java.lang.String)
+         */
+        public boolean addScript(String script)
+        {
+            try
+            {
+                synchronized (lock)
+                {
+                    lock.notifyAll();
+                }
+            }
+            catch (Exception ex)
+            {
+                log.warn("Failed to notify all ScriptSession users", ex); //$NON-NLS-1$
+            }
+
+            // We havn't done anything with the script, so
+            return false;
+        }
+
+        /* (non-Javadoc)
+         * @see org.directwebremoting.ScriptConduit#flush()
+         */
+        public void flush()
+        {
+        }
+
+        private final Object lock;
+    }
+
+    /**
+     * Implementaion of ScriptConduit that just resumes a continuation.
+     */
+    private static final class ResumeContinuationScriptConduit implements ScriptConduit
+    {
         /**
          * @param continuation
          */
-        private SystemScriptConduit(Object continuation)
+        private ResumeContinuationScriptConduit(Object continuation)
         {
             this.continuation = continuation;
         }
@@ -209,6 +244,8 @@ public class DwrSystem
         public void flush()
         {
         }
+
+        private final Object continuation;
     }
 
     /**
