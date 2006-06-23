7a21b6a0dffd12e391f9578cfcafb8235886fca2
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-06-23 15:08:23 +0000

    new examples
    git-svn-id: http://svn.directwebremoting.org/trunk@734 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- demo/uk/ltd/getahead/dwrdemo/chat/Message.java
+++ demo/uk/ltd/getahead/dwrdemo/chat/Message.java
@@ -1,5 +1,8 @@
 package uk.ltd.getahead.dwrdemo.chat;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
@@ -11,12 +14,26 @@ public class Message
     public Message(String newtext)
     {
         text = newtext;
+
         if (text.length() > 256)
         {
             text = text.substring(0, 256);
         }
         text = text.replace('<', '[');
         text = text.replace('&', '_');
+
+        try
+        {
+            if (text.startsWith("http://")) //$NON-NLS-1$
+            {
+                URL url = new URL(text);
+                text = "<a href='#' onclick='window.open(\"" + url.toExternalForm() + "\", \"\", \"resizable=yes,scrollbars=yes,status=yes\");'>" + url.toExternalForm() + "</a>"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            }
+        }
+        catch (MalformedURLException ex)
+        {
+            // Ignore - it's not a URL
+        }
     }
 
     /**
