1196cdf01e958bdefb463e58dc147f7daad1ddc0
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-07-01 11:48:51 +0000

    reviewed and cleaned-up
    git-svn-id: http://svn.directwebremoting.org/trunk@769 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/org/directwebremoting/create/Ejb3Creator.java
+++ java/org/directwebremoting/create/Ejb3Creator.java
@@ -4,41 +4,28 @@ import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.directwebremoting.Creator;
 import org.directwebremoting.util.Messages;
 
 /**
- * 
+ * A Creator that works against EJB3 beans 
+ * @author Squishy [Squishy_I at gmx dot net]
+ * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
 public class Ejb3Creator extends AbstractCreator implements Creator
 {
-
-    public final static String LOCAL = "local";
-
-    //  remote interface by default
-    boolean remote = true;
-
-    String bean;
-
-    String clazz;
-
-    String beanend = "Bean";
-
     /**
      * The common interface of the Bean.
-     * <b>If you don't have a common interface
-     * from which local and remote are derived, you have to set the bean
-     * name manually!</b>
-     * The Beanname is gotten from the part of the String behind the last '.'
+     * <b>If you don't have a common interface from which local and remote are
+     * derived, you have to set the bean name manually!</b>
+     * The BeanName is fetched from the part of the String behind the last '.'
      * @param clazz The fully qualified classname of the Bean's interface
-     *
      */
     public void setInterface(String clazz)
     {
         this.clazz = clazz;
-        this.bean = clazz.substring(clazz.lastIndexOf(".") + 1);
+        this.bean = clazz.substring(clazz.lastIndexOf('.') + 1);
     }
 
     /**
@@ -53,68 +40,81 @@ public class Ejb3Creator extends AbstractCreator implements Creator
     /**
      * Get local or remote interface?
      * Defaults remote
-     * @param interfazz Either "local" for the local interface or anything else for the remote
+     * @param iface Either "local" for the local interface or anything else for the remote
      */
-    public void setInterfaceType(String interfazz)
+    public void setInterfaceType(String iface)
     {
-        if (interfazz.equalsIgnoreCase(LOCAL))
-            remote = false;
-        else
-            remote = true;
+        remote = !iface.equalsIgnoreCase(LOCAL);
     }
 
     /**
-     * Set the ending that is appended to the actual bean name gotten from the
-     * interface name (UserMgr --> UserMgrBean)
-     *
-     * defaults to "Bean"
-     *  @param beanend
+     * Set the ending that is appended to the actual bean name fetched from the
+     * interface name (UserMgr --> UserMgrBean). Defaults to "Bean"
+     * @param beanNamePostfix The name to append to a bean before lookup
      */
-    public void setBeanEnd(String beanend)
+    public void setBeanNamePostfix(String beanNamePostfix)
     {
-        this.beanend = beanend;
+        this.beanNamePostfix = beanNamePostfix;
     }
 
+    /* (non-Javadoc)
+     * @see org.directwebremoting.Creator#getType()
+     */
     public Class getType()
     {
         try
         {
             return Class.forName(clazz);
         }
-        catch (ClassNotFoundException e)
+        catch (ClassNotFoundException ex)
         {
-            throw new IllegalArgumentException(Messages.getString("Creator.BeanClassNotFound", clazz));
+            throw new IllegalArgumentException(Messages.getString("Creator.BeanClassNotFound", clazz)); //$NON-NLS-1$
         }
     }
 
+    /* (non-Javadoc)
+     * @see org.directwebremoting.Creator#getInstance()
+     */
     public Object getInstance() throws InstantiationException
     {
-        String type = remote ? "remote" : "local";
+        String type = remote ? "remote" : "local"; //$NON-NLS-1$ //$NON-NLS-2$
+
         try
         {
-            Context jndi = getInitialContext();
-            Object o = jndi.lookup(bean + beanend + "/" + type);
-            return o;
+            Properties props = new Properties();
+            props.load(getClass().getResourceAsStream("/jndi.properties")); //$NON-NLS-1$
+            Context jndi = new InitialContext(props);
+
+            return jndi.lookup(bean + beanNamePostfix + "/" + type); //$NON-NLS-1$
         }
-        catch (NamingException e)
+        catch (Exception ex)
         {
-            throw new InstantiationException(bean + "/" + type + " not bound!");
+            throw new InstantiationException(bean + "/" + type + " not bound:" + ex.getMessage()); //$NON-NLS-1$ //$NON-NLS-2$
         }
     }
 
-    protected Context getInitialContext()
-    {
+    /**
+     * Constant for local/remote lookup
+     */
+    private final static String LOCAL = "local"; //$NON-NLS-1$
 
-        Properties props = new Properties();
-        try
-        {
-            props.load(getClass().getResourceAsStream("/jndi.properties"));
-            return new InitialContext(props);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace(System.err);
-        }
-        return null;
-    }
+    /**
+     * Are we using a remote interface? Default == true
+     */
+    private boolean remote = true;
+
+    /**
+     * The name of the bean
+     */
+    private String bean;
+
+    /**
+     * The type of the bean
+     */
+    private String clazz;
+
+    /**
+     * A suffix to help lookup
+     */
+    private String beanNamePostfix = "Bean"; //$NON-NLS-1$
 }
