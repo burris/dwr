ca9f706b0f4763b3f891a9888d4c3d3e132cd9b4
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-07-09 22:17:26 +0000

    fixing faces cleanup bugs
    git-svn-id: http://svn.directwebremoting.org/trunk@778 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/org/directwebremoting/faces/FacesExtensionFilter.java
+++ java/org/directwebremoting/faces/FacesExtensionFilter.java
@@ -30,6 +30,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.directwebremoting.util.Logger;
+
 /**
  * Filter integration for DWR framework. This filter was inspired by this
  * article: http://www.thoughtsabout.net/blog/archives/000033.html
@@ -71,8 +73,24 @@ public class FacesExtensionFilter implements Filter
         // Set using our inner class
         InnerFacesContext.setFacesContextAsCurrentInstance(facesContext);
 
-        // call the filter chain
-        chain.doFilter(request, response);
+        try
+        {
+            // call the filter chain
+            chain.doFilter(request, response);
+        }
+        finally
+        {
+            // Clean up after ourselves as FacesContext is a ThreadLocal object
+            try
+            {
+                facesContext.release();
+            }
+            catch (IllegalStateException ex)
+            {
+                // Perhaps the FacesContext has already been released?
+                log.warn("Double release of faces context?", ex); //$NON-NLS-1$
+            }
+        }
     }
 
     /* (non-Javadoc)
@@ -93,5 +111,13 @@ public class FacesExtensionFilter implements Filter
         }
     }
 
+    /**
+     * The servlet context
+     */
     private ServletContext servletContext = null;
+
+    /**
+     * The log stream
+     */
+    private static final Logger log = Logger.getLogger(FacesExtensionFilter.class);
 }
