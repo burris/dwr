9309b09b58dc98f28f6df2db6a950c25ae3ebda4
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2007-02-09 09:29:39 +0000

    fix silly compile error
    git-svn-id: http://svn.directwebremoting.org/trunk@1131 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java5/org/directwebremoting/annotations/AnnotationsConfigurator.java
+++ java5/org/directwebremoting/annotations/AnnotationsConfigurator.java
@@ -20,6 +20,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
