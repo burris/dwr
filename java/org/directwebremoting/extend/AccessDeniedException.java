1bd4640f410a25fa4c4bd4a41b3f107f86d10252
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2007-03-31 19:41:38 +0000

    Improve comments Issue number:  DWR-50
    git-svn-id: http://svn.directwebremoting.org/trunk@1202 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/org/directwebremoting/extend/AccessDeniedException.java
+++ java/org/directwebremoting/extend/AccessDeniedException.java
@@ -16,11 +16,13 @@
 package org.directwebremoting.extend;
 
 /**
+ * TODO: Work out if this exception actually helps.
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
 public class AccessDeniedException extends SecurityException
 {
     /**
+     * Basic constructor - ensure all exceptions have messages
      * @param s The exception message
      */
     public AccessDeniedException(String s)
