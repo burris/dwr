bd060a78e85fa36844a9032e941090cc66e85c8e
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2007-06-01 09:22:52 +0000

    More user friendly error message
    git-svn-id: http://svn.directwebremoting.org/trunk@1365 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/org/directwebremoting/engine.js
+++ java/org/directwebremoting/engine.js
@@ -193,8 +193,7 @@ dwr.engine.setPollType = function(newPollType) {
  */
 dwr.engine.defaultErrorHandler = function(message, ex) {
   dwr.engine._debug("Error: " + ex.name + ", " + ex.message, true);
-
-  if (message == null || message == "") alert("A server error has occured. More information may be available in the console.");
+  if (message == null || message == "") alert("A server error has occured.");
   // Ignore NS_ERROR_NOT_AVAILABLE if Mozilla is being narky
   else if (message.indexOf("0x80040111") != -1) dwr.engine._debug(message);
   else alert(message);
