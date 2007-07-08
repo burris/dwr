8a06bdd3320ad8a68a11c90208c24eaea031b2a2
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2007-07-08 21:57:07 +0000

    add the ability to create different sizes of random people sets
    git-svn-id: http://svn.directwebremoting.org/trunk@1436 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- demo/org/getahead/dwrdemo/people/People.java
+++ demo/org/getahead/dwrdemo/people/People.java
@@ -15,12 +15,14 @@
  */
 package org.getahead.dwrdemo.people;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
 
-import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.getahead.dwrdemo.util.RandomData;
 
 /**
  * A container for a set of people
@@ -33,8 +35,15 @@ public class People
      */
     public People()
     {
-        log.debug("Generating a new set of random people");
-        for (int i = 0; i < 5; i++)
+        init(5);
+    }
+
+    /**
+     * 
+     */
+    public void init(int count)
+    {
+        for (int i = 0; i < count; i++)
         {
             people.add(getRandomPerson());
         }
@@ -44,9 +53,28 @@ public class People
      * Accessor for the current list of people
      * @return the current list of people
      */
-    public Set<Person> getAllPeople()
+    public List<Person> getAllPeople()
     {
         return people;
+    }
+
+    /**
+     * Accessor for a subset of the current list of people
+     * @return the current list of people
+     */
+    public List<Person> getMatchingPeople(String filter)
+    {
+        List<Person> reply = new ArrayList<Person>();
+        Pattern regex = Pattern.compile(filter, Pattern.CASE_INSENSITIVE);
+        for (Person person : people)
+        {
+            if (regex.matcher(person.getName()).find())
+            {
+                reply.add(person);
+                log.debug("Adding " + person + " to reply");
+            }
+        }
+        return reply;
     }
 
     /**
@@ -77,33 +105,18 @@ public class People
     }
 
     /**
-     * the current list of people
-     */
-    private Set<Person> people = new HashSet<Person>();
-
-    /**
      * Create a random person
      * @return a random person
      */
-    private Person getRandomPerson()
+    public static Person getRandomPerson()
     {
         Person person = new Person();
         person.setId(getNextId());
-
-        String firstname = FIRSTNAMES[random.nextInt(FIRSTNAMES.length)];
-        String surname = SURNAMES[random.nextInt(SURNAMES.length)];
-        person.setName(firstname + " " + surname);
-
-        String housenum = (random.nextInt(99) + 1) + " ";
-        String road1 = ROADS1[random.nextInt(ROADS1.length)];
-        String road2 = ROADS2[random.nextInt(ROADS2.length)];
-        String town = TOWNS[random.nextInt(TOWNS.length)];
-        String address = housenum + road1 + " " + road2 + ", " + town;
-        person.setAddress(address);
-
-        float salary = Math.round(10 + 90 * random.nextFloat()) * 1000;
-        person.setSalary(salary);
-
+        person.setName(RandomData.getFullName());
+        String[] addressAndNumber = RandomData.getAddressAndNumber();
+        person.setAddress(addressAndNumber[0]);
+        person.setPhoneNumber(addressAndNumber[1]);
+        person.setSalary(RandomData.getSalary());
         return person;
     }
 
@@ -122,7 +135,7 @@ public class People
      * Get the next unique ID in a thread safe way
      * @return a unique id
      */
-    private static synchronized int getNextId()
+    public static synchronized int getNextId()
     {
         return nextId++;
     }
@@ -132,35 +145,10 @@ public class People
      */
     private static int nextId = 1;
 
-    private Random random = new Random();
-
-    private static final String[] FIRSTNAMES =
-    {
-        "Fred", "Jim", "Shiela", "Jack", "Betty", "Jacob", "Martha", "Kelly",
-        "Luke", "Matt", "Gemma", "Joe", "Ben", "Jessie", "Leanne", "Becky",
-    };
-
-    private static final String[] SURNAMES =
-    {
-        "Sutcliffe", "MacDonald", "Duckworth", "Smith", "Wisner", "Iversen",
-        "Nield", "Turton", "Trelfer", "Wilson", "Johnson", "Cowan", "Daniels",
-    };
-
-    private static final String[] ROADS1 =
-    {
-        "Green", "Red", "Yellow", "Brown", "Blue", "Black", "White",
-    };
-
-    private static final String[] ROADS2 =
-    {
-        "Close", "Drive", "Street", "Avenue", "Crescent", "Road", "Place",
-    };
-
-    private static final String[] TOWNS =
-    {
-        "Birmingham", "Kettering", "Paris", "San Francisco", "New York",
-        "San Mateo", "Barcelona",
-    };
+    /**
+     * the current list of people
+     */
+    private List<Person> people =  Collections.synchronizedList(new ArrayList<Person>());
 
     /**
      * The log stream
