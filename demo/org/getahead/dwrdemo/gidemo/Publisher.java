8ca089f32fbb28f4f67f7ce0afbbe898688ceb18
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-02-26 14:38:43 +0000

    GI demo publisher should use Scheduler rather than a custom thread
    git-svn-id: http://svn.directwebremoting.org/trunk@1672 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- demo/org/getahead/dwrdemo/gidemo/Publisher.java
+++ demo/org/getahead/dwrdemo/gidemo/Publisher.java
@@ -16,109 +16,50 @@
 package org.getahead.dwrdemo.gidemo;
 
 import java.util.Collection;
-import java.util.Random;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-import javax.servlet.ServletContext;
-
-import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.Log;
 import org.directwebremoting.ScriptSession;
 import org.directwebremoting.ServerContext;
 import org.directwebremoting.ServerContextFactory;
-import org.directwebremoting.WebContext;
-import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.proxy.ScriptProxy;
+import org.directwebremoting.util.SharedObjects;
 
 /**
  * A generator of random objects to push to GI
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
-public class Publisher implements Runnable
+public class Publisher
 {
     /**
      * Create a new publish thread and start it
      */
     public Publisher()
     {
-        WebContext webContext = WebContextFactory.get();
-        ServletContext servletContext = webContext.getServletContext();
-
-        serverContext = ServerContextFactory.get(servletContext);
-
-        // A bit nasty: the call to serverContext.getScriptSessionsByPage()
-        // below could fail because the system might need to read web.xml which
-        // means it needs a ServletContext, which is only available using
-        // WebContext, which in turn requires a DWR thread. We can cache the
-        // results simply by calling this in a DWR thread, as we are now.
-        webContext.getScriptSessionsByPage("");
-
-        synchronized (Publisher.class)
-        {
-            if (worker == null)
-            {
-                worker = new Thread(this, "Publisher");
-                worker.start();
-            }
-        }
+        Runnable runnable = new PublisherRunnable();
+        ScheduledThreadPoolExecutor executor = SharedObjects.getScheduledThreadPoolExecutor();
+        executor.scheduleAtFixedRate(runnable, 1, 1, TimeUnit.SECONDS);
     }
 
-    /* (non-Javadoc)
-     * @see java.lang.Runnable#run()
-     */
-    public void run()
+    protected class PublisherRunnable implements Runnable
     {
-        try
+        /* (non-Javadoc)
+         * @see java.lang.Runnable#run()
+         */
+        public void run()
         {
-            log.info("Starting Publisher thread");
+            ServerContext serverContext = ServerContextFactory.get();
+            String contextPath = serverContext.getContextPath();
+            Collection<ScriptSession> sessions = serverContext.getScriptSessionsByPage(contextPath + "/gi/dwr-oa-gi.html");
+            ScriptProxy proxy = new ScriptProxy(sessions);
 
-            while (!Thread.currentThread().isInterrupted())
-            {
-                String contextPath = serverContext.getContextPath();
-                if (contextPath != null)
-                {
-                    Collection<ScriptSession> sessions = serverContext.getScriptSessionsByPage(contextPath + "/gi/dwr-oa-gi.html");
-                    ScriptProxy proxy = new ScriptProxy(sessions);
-    
-                    Corporation corp = corporations.getNextChangedCorporation();
-                    proxy.addFunctionCall("OpenAjax.hub.publish", "gidemo.corp", corp);
-                }
-
-                int timeToSleep = random.nextInt(1000);
-                Thread.sleep(timeToSleep);
-            }
-        }
-        catch (InterruptedException ex)
-        {
-            // Ignore, we expect this
-        }
-        finally
-        {
-            log.info("Stopping Publisher thread");
+            Corporation corp = corporations.getNextChangedCorporation();
+            proxy.addFunctionCall("OpenAjax.hub.publish", "gidemo.corp", corp);
         }
     }
-
-    /**
-     * The thread that does the work
-     */
-    protected static Thread worker;
 
     /**
      * The set of corporations that we manage
      */
-    private Corporations corporations = new Corporations();
-
-    /**
-     * We use DWRs ServerContext to find users of a given page
-     */
-    private ServerContext serverContext;
-
-    /**
-     * Used to generate random data
-     */
-    private Random random = new Random();
-
-    /**
-     * The log stream
-     */
-    private static final Log log = LogFactory.getLog(Publisher.class);
+    protected Corporations corporations = new Corporations();
 }
