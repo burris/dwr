76894ec94c8b5f413b1bc159ec700f9878fbba2a
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-03-19 15:31:19 +0000

    Addition of more window alteration methods
    git-svn-id: http://svn.directwebremoting.org/trunk@1745 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/org/directwebremoting/proxy/browser/Window.java
+++ java/org/directwebremoting/proxy/browser/Window.java
@@ -21,6 +21,8 @@ import java.util.Collection;
 
 import org.directwebremoting.ScriptBuffer;
 import org.directwebremoting.ScriptSession;
+import org.directwebremoting.extend.CallbackHelper;
+import org.directwebremoting.proxy.Callback;
 import org.directwebremoting.proxy.ScriptProxy;
 
 /**
@@ -65,20 +67,104 @@ public class Window extends ScriptProxy
         addFunctionCall("alert", message);
     }
 
+    /**
+     * Show a 'confirm' dialog
+     * @param message The text to go into the alert box
+     * @param callback The function to be called when a browser replies
+     */
+    public void confirm(String message, Callback<Boolean> callback)
+    {
+        ScriptBuffer script = new ScriptBuffer();
+        String callbackPrefix = "";
+
+        if (callback != null)
+        {
+            callbackPrefix = "var reply = ";
+        }
+
+        script.appendCall(callbackPrefix + "confirm", message);
+
+        if (callback != null)
+        {
+            String key = CallbackHelper.saveCallback(callback, Boolean.class);
+            script.appendCall("__System.activateCallback", key, "reply");
+        }
+
+        addScript(script);
+    }
+
+   /**
+     * Show a 'prompt' dialog
+     * @param message The text to go into the alert box
+     * @param callback The function to be called when a browser replies
+     */
+    public void prompt(String message, Callback<String> callback)
+    {
+        ScriptBuffer script = new ScriptBuffer();
+        String callbackPrefix = "";
+
+        if (callback != null)
+        {
+            callbackPrefix = "var reply = ";
+        }
+
+        script.appendCall(callbackPrefix + "prompt", message);
+
+        if (callback != null)
+        {
+            String key = CallbackHelper.saveCallback(callback, String.class);
+            script.appendCall("__System.activateCallback", key, "reply");
+        }
+
+        addScript(script);
+    }
+
+    /**
+     * Attempt to close this window
+     */
+    public void close()
+    {
+        addFunctionCall("close");
+    }
+
+    /**
+     * Navigate to a new page
+     * @param newPage The page to navigate to
+     */
     public void setLocation(URI newPage)
     {
         setLocation(newPage.toASCIIString());
     }
 
+    /**
+     * Navigate to a new page
+     * @param newPage The page to navigate to
+     */
     public void setLocation(URL newPage)
     {
         setLocation(newPage.toExternalForm());
     }
 
+    /**
+     * Navigate to a new page
+     * @param newPage The page to navigate to
+     */
     public void setLocation(String newPage)
     {
         ScriptBuffer script = new ScriptBuffer();
         script.appendData("window.location = '" + newPage + "';");
         addScript(script);
     }
+
+    /*
+     * This would be good, but it doesn't work on some browsers
+     * @param title The page to navigate to
+     *
+    public void setTitle(String title)
+    {
+        ScriptBuffer script = new ScriptBuffer();
+        script.appendData("window.document.title = '" + title + "';");
+        addScript(script);
+    }
+    */
 }
